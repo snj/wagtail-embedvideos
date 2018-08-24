5fe0be12d0e49c5e029cabf56265013633d064fd
KINOSHITA Shinji <shinji-kinoshita@happy-science.org>
2018-08-24 19:52:10 +0900

    s/wagtailcore/wagtail.core/ on migration 0003

--- wagtail_embed_videos/migrations/0003_embedvideo_collection.py
+++ wagtail_embed_videos/migrations/0003_embedvideo_collection.py
@@ -17,6 +17,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='embedvideo',
             name='collection',
-            field=models.ForeignKey(default=wagtail.core.models.get_root_collection_id, on_delete=django.db.models.deletion.CASCADE, related_name='+', to='wagtailcore.Collection', verbose_name='collection'),
+            field=models.ForeignKey(default=wagtail.core.models.get_root_collection_id, on_delete=django.db.models.deletion.CASCADE, related_name='+', to='wagtail.core.Collection', verbose_name='collection'),
         ),
     ]
